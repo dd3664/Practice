0b6865066c6e4057d963b115058d1d8dd6fb3f94
dongdong <scudongdong@163.com>
2023-10-29 09:36:34 -0700

    日志文件工具

--- test/logTool_test/include/logTool.h
+++ test/logTool_test/include/logTool.h
@@ -24,44 +24,54 @@
 #define SUFFIX_LEN                              10
 #define MAX_PATH_LEN                            64
 #define MAX_CMD_LEN                             128
-#define ROTATE_FILE(fp) do \
-						{ \
-							int n = 0; \
-							char oldPath[MAX_PATH_LEN] = {0}, newPath[MAX_PATH_LEN] = {0}, tarCmd[MAX_CMD_LEN] = {0}; \
-							size_t base = strlen(TPLOG_PATH); \
-							fclose(fp); \
-							memcpy(oldPath, TPLOG_PATH, base); \
-							memcpy(newPath, TPLOG_PATH, base); \
-							sprintf(tarCmd, "tar -czvPf %s%s %s", TPLOG_PATH, TAR_SUFFIX, TPLOG_PATH); \
-							printf("TPT tarCmd:%s\n", tarCmd); \
-							system(tarCmd); \
-							for (n = MAX_ROTATE - 1; n >= 0; n--) \
-							{ \
-								snprintf(oldPath + base, SUFFIX_LEN, n ? "%s.%d" : "%s",TAR_SUFFIX,  n - 1); \
-								snprintf(newPath + base, SUFFIX_LEN, "%s.%d",TAR_SUFFIX, n); \
-								printf("TPT oldPath:%s, newPath:%s\n", oldPath, newPath); \
-								if (access(newPath, F_OK) == 0) \
-									remove(newPath); \
-								if (access(oldPath, F_OK) == 0) \
-									rename(oldPath, newPath); \
-							} \
-							remove(TPLOG_PATH); \
-							fp = fopen(TPLOG_PATH, "a+"); \
-						} while (0);
+#define MAX_TIME_LEN                            24
+
+#define GET_TIME(timeStr) do \
+						 { \
+						 	time_t tt = time(NULL); \
+							struct tm *ptm = localtime(&tt); \
+							strftime(timeStr, MAX_TIME_LEN, "%Y-%m-%d %H:%M:%S", ptm); \
+						 } while (0);
+#define TAR_AND_ROTATE_FILE(fp) do \
+								{ \
+									int n = 0; \
+									char oldPath[MAX_PATH_LEN] = {0}, newPath[MAX_PATH_LEN] = {0}, tarCmd[MAX_CMD_LEN] = {0}; \
+									size_t base = strlen(TPLOG_PATH); \
+									fclose(fp); \
+									memcpy(oldPath, TPLOG_PATH, base); \
+									memcpy(newPath, TPLOG_PATH, base); \
+									sprintf(tarCmd, "tar -czvPf %s%s %s", TPLOG_PATH, TAR_SUFFIX, TPLOG_PATH); \
+									printf("TPT tarCmd:%s\n", tarCmd); \
+									system(tarCmd); \
+									for (n = MAX_ROTATE - 1; n >= 0; n--) \
+									{ \
+										snprintf(oldPath + base, SUFFIX_LEN, n ? "%s.%d" : "%s",TAR_SUFFIX,  n - 1); \
+										snprintf(newPath + base, SUFFIX_LEN, "%s.%d",TAR_SUFFIX, n); \
+										printf("TPT oldPath:%s, newPath:%s\n", oldPath, newPath); \
+										if (access(newPath, F_OK) == 0) \
+											remove(newPath); \
+										if (access(oldPath, F_OK) == 0) \
+											rename(oldPath, newPath); \
+									} \
+									remove(TPLOG_PATH); \
+									fp = fopen(TPLOG_PATH, "a+"); \
+								} while (0);
 
 #define TPLOG(fmt, ...) do \
 						{ \
 							FILE *fp = fopen(TPLOG_PATH, "a+"); \
 							size_t file_size = 0; \
+							char timeStr[MAX_TIME_LEN] = {0}; \
 							if (fp == NULL) \
 								break; \
 							fseek(fp, 0L, SEEK_END); \
 							file_size = ftell(fp); \
 							if (file_size >= MAX_SIXE) \
 							{ \
-								ROTATE_FILE(fp) \
+								TAR_AND_ROTATE_FILE(fp) \
 							} \
-							fprintf(fp, "[%s:%d] "fmt"\n", __FUNCTION__, __LINE__, ##__VA_ARGS__); \
+							GET_TIME(timeStr) \
+							fprintf(fp, "[%s] [%s:%s:%d] "fmt"\n",timeStr, __FILE__,  __FUNCTION__, __LINE__, ##__VA_ARGS__); \
 							fclose(fp); \
 						} while(0);
 /****************************************************************************************************/
