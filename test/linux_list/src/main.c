ce8100e295ec45d1983e8f78c08911f3f85ea336
dongdong <scudongdong@163.com>
2024-05-29 09:51:45 -0700

    移植linux内核list_sort排序

--- test/linux_list/src/main.c
+++ test/linux_list/src/main.c
@@ -5,6 +5,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include "list.h"
+#include "list_sort.h"
 /****************************************************************************************************/
 /*                                           DEFINES                                                */
 /****************************************************************************************************/
@@ -20,6 +21,19 @@ LIST_HEAD(test_list);
 /****************************************************************************************************/
 /*                                       STATIC FUNCTIONS                                           */
 /****************************************************************************************************/
+static int descending_sort(void *priv, struct list_head *a, struct list_head *b)
+{
+	LISTNODE *list_node_a = list_entry(a, LISTNODE, list);
+	LISTNODE *list_node_b = list_entry(b, LISTNODE, list);
+	return (list_node_b->data - list_node_a->data);
+}
+
+static int ascending_sort(void *priv, struct list_head *a, struct list_head *b)
+{
+	LISTNODE *list_node_a = list_entry(a, LISTNODE, list);
+	LISTNODE *list_node_b = list_entry(b, LISTNODE, list);
+	return (list_node_a->data - list_node_b->data);
+}
 
 /****************************************************************************************************/
 /*                                       PUBLIC FUNCTIONS                                           */
@@ -28,6 +42,7 @@ int main(int argc, char *argv[])
 {	
 	LISTNODE *node;
 	LISTNODE *tmp;
+	int test[] = {9, 1, 8, 4, 7};
 	int i;
 
 	for (i = 0; i < 5; i++)
@@ -38,15 +53,30 @@ int main(int argc, char *argv[])
 			break;
 		}
 		memset(node, 0, sizeof(LISTNODE));
-		node->data = i;
+		node->data = test[i];
 		INIT_LIST_HEAD(&node->list);
 		list_add_tail(&node->list, &test_list);
 	}
 
+	list_for_each_entry(node, &test_list, list)
+	{
+		printf("before sort, Data=%d\n", node->data);
+	}
+
+	list_sort(NULL, &test_list, descending_sort);
+
+	list_for_each_entry(node, &test_list, list)
+	{
+		printf("descending sort, Data=%d\n", node->data);
+	}
+
+	list_sort(NULL, &test_list, ascending_sort);
+	
 	list_for_each_entry_safe(node, tmp, &test_list, list)
 	{
-		printf("Data=%d\n", node->data);
+		printf("ascending sort, Data=%d \n", node->data);
 		free(node);
 	}
+
 	return 0;
 }
